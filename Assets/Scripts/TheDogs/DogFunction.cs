5d683aa8ea277496177b76a1ee10f6af190b7727
Casey <cb@shotgunflat.com>
2017-07-05 23:15:39 -0400

    Basic movement and ?jump?

--- Assets/Scripts/TheDogs/DogFunction.cs
+++ Assets/Scripts/TheDogs/DogFunction.cs
@@ -4,13 +4,51 @@ using UnityEngine;
 
 public class DogFunction : MonoBehaviour {
 
-	// Use this for initialization
+
+    private float speed = 6.0f;
+    private float jumpHeight = 4.0f;
+
+    private Rigidbody2D body;
+    private CharacterController dog;
+
+    private Vector3 lastPos;
+
+
 	void Start () {
-		
+        body = GetComponent<Rigidbody2D>();
+
+        if (body) {
+            body.freezeRotation = true;
+        }
+
+        dog = GetComponent<CharacterController>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+        float movX = Input.GetAxis("Horizontal") * speed;
+
+        Vector3 movement = new Vector3(movX, 0, 0);
+        movement = Vector3.ClampMagnitude(movement, speed);
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+
+        transform.Translate(movement);
+
+        lastPos = transform.position;
+        Debug.Log(Input.GetAxis("Jump"));
+        if (Input.GetAxis("Jump") != 0) {
+            transform.Translate(0, Input.GetAxis("Jump"), 0);
+        }
 	}
+
+    private void OnTriggerStay2D(Collider2D collision) {
+        if (collision.transform.gameObject.layer == 8) {
+            
+        }
+        Debug.Log(collision.transform.gameObject.layer);
+
+        if (collision.transform.gameObject.layer == 9) {
+        }
+    }
+
 }
