5263594042349ad9ec029254f670e8bfd173bebd
Casey <cb@shotgunflat.com>
2017-07-07 17:04:06 -0400

    I don't know if i did anything

---
+++ Assets/Scripts/TheGuns/BasicGunProperties.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicGunProperties : MonoBehaviour {
+
+	float sens = 20.0f;
+
+	void Start () {
+	}
+
+	void Update () {
+		Vector3 target = Input.mousePosition;
+		transform.right = target - transform.position;
+	}
+}
