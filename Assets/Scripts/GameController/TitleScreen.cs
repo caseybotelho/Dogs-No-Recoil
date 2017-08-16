f4e450834c435a2b3f7e2dc55b35a7c39aabd252
Casey <cb@shotgunflat.com>
2017-08-15 23:38:37 -0400

    quick script

---
+++ Assets/Scripts/GameController/TitleScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreen : MonoBehaviour {
+    
+    [SerializeField] private GameObject n;
+    [SerializeField] private GameObject o;
+    [SerializeField] private GameObject r;
+    [SerializeField] private GameObject e;
+    [SerializeField] private GameObject c;
+    [SerializeField] private GameObject _o;
+    [SerializeField] private GameObject i;
+    [SerializeField] private GameObject l;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
