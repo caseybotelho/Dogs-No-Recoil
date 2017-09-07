d225a0a17ed7578107d5a4f63e50dbd752c7245c
Casey <cb@shotgunflat.com>
2017-09-07 17:24:13 -0400

    Title screen animation/sound

--- Assets/Scripts/GameController/TitleScreen.cs
+++ Assets/Scripts/GameController/TitleScreen.cs
@@ -3,21 +3,71 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TitleScreen : MonoBehaviour {
-    
+
+	[SerializeField] private GameObject dogs;
+
     [SerializeField] private GameObject n;
     [SerializeField] private GameObject o;
     [SerializeField] private GameObject r;
     [SerializeField] private GameObject e;
     [SerializeField] private GameObject c;
-    [SerializeField] private GameObject _o;
+    [SerializeField] private GameObject o_;
     [SerializeField] private GameObject i;
     [SerializeField] private GameObject l;
 
+	private AudioSource noise;
+
+	private const int arraySize = 2;
+	[SerializeField] AudioClip[] noises = new AudioClip[arraySize];
+
 	void Start () {
-		
+		StartCoroutine (Title ());
+
+		noise = GetComponent<AudioSource>();
+		noise.clip = noises [0];
 	}
 	
 	void Update () {
 		
 	}
+
+	private IEnumerator Title() {
+		yield return new WaitForSeconds (1.0f);
+		dogs.SetActive (true);
+		noise.Play ();
+
+		yield return new WaitForSeconds (2.0f);
+		noise.clip = noises [1];
+		n.SetActive (true);
+		noise.Play ();
+
+		yield return new WaitForSeconds (0.25f);
+		o.SetActive (true);
+		noise.Play ();
+
+		yield return new WaitForSeconds (0.25f);
+		r.SetActive (true);
+		noise.Play ();
+
+		yield return new WaitForSeconds (0.25f);
+		e.SetActive (true);
+		noise.Play ();
+
+		yield return new WaitForSeconds (0.25f);
+		c.SetActive (true);
+		noise.Play ();
+
+		yield return new WaitForSeconds (0.25f);
+		o_.SetActive (true);
+		noise.Play ();
+
+		yield return new WaitForSeconds (0.25f);
+		i.SetActive (true);
+		noise.Play ();
+
+		yield return new WaitForSeconds (0.25f);
+		l.SetActive (true);
+		noise.Play ();
+	}
+
 }
